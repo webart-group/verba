569eded35311a26a0cd80b90e2c496765f0ca1ef
kmv <kmv@webart.group>
2023-09-27 18:44:01 +0300

    ServicePromo update

--- src/Mod/Service/Transformers/ServicePromo.php
+++ src/Mod/Service/Transformers/ServicePromo.php
@@ -4,13 +4,25 @@
 namespace Verba\Mod\Service\Transformers;
 
 
+use function Verba\_mod;
+
 class ServicePromo
 {
     public function transform($item)
     {
+        $mImage = _mod('image');
+
+        if (!empty($item['picture'])) {
+            $imgCfg = $mImage->getImageConfig('content');
+            $picture = $imgCfg->getFullUrl(basename($item['picture']));
+        }else{
+            $picture = null;
+        }
+
         return [
             'title_preview' => $item['title_preview'] ?? null,
             'text_preview' => $item['text_preview'] ?? null,
+            'pucture' => $picture,
         ];
     }
 }

498194c4b2bcb6eba2050a946a2e42083ca925d4
Anton <anton.kolomiets98@gmail.com>
2023-10-10 20:35:26 +0300

    Telegram notification added

---
+++ src/Mod/Telegram.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Verba\Mod;
+
+use Verba\Mod\SnailMail\Email;
+
+class Telegram extends \Verba\Mod
+{
+
+    use \Verba\ModInstance;
+
+    function notifyAdmins($message)
+    {
+        // Список всех пользователей, для которых идет рассылка
+        $subscribers = [
+              [$this->_c['admins_chat_id']], // https://t.me/+EN9gv5EGLyRjYTgy Boostify Notification Group
+        ];
+
+        // Отправить уведомление каждому
+        foreach ($subscribers as $subscriber) {
+            $apiUrl = 'https://api.telegram.org/bot' . $this->_c['token'] . '/sendMessage';
+
+            $postData = [
+                'chat_id' => $subscriber['chat_id'],
+                'text' => $message,
+            ];
+
+            $ch = curl_init($apiUrl);
+            curl_setopt_array($ch, [
+                CURLOPT_POST => TRUE,
+                CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_TIMEOUT => 10,
+                CURLOPT_POSTFIELDS => $postData,
+            ]);
+
+            $response = curl_exec($ch);
+
+            if (curl_errno($ch)) {
+                echo 'Ошибка при отправке уведомления для chat_id ' . $subscriber['chat_id'] . ': ' . curl_error($ch) . PHP_EOL;
+            } else {
+                print_r('Уведомление успешно отправлено для chat_id ' . $subscriber['chat_id'] . "\n");
+            }
+
+            curl_close($ch);
+        }
+    }
+
+}
